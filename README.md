cebc016aa54614863e422ddcc2016394e7381d01
jefree <jgarzon920429@gmail.com>
2015-04-27 19:35:43 -0500

    first commit: dev environment configuration

---
+++ README.md
@@ -0,0 +1,11 @@
+Please install gulp and stylues for use this repo:
+
+  > npm install -g gulp stylus
+
+Then install the dependencies:
+
+  > npm install
+
+and run the server:
+
+  > gulp
---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var server = require('gulp-express');
+var stylus = require('gulp-stylus');
+
+gulp.task('styles', function(){
+
+  gulp
+    .src('./views/styles/**/*.stylus')
+    .pipe(stylus())
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('default', function() {
+  
+  gulp.run('styles');
+  server.run(['./bin/www']);
+
+  gulp.watch(['./public/js/**/*.js', './views/**/*.jade'], server.notify);
+  gulp.watch(['./views/styles/**/*.stylus'], ['styles', server.notify]);
+  gulp.watch(['./routes/**/*.js'], [server.run]);
+});
